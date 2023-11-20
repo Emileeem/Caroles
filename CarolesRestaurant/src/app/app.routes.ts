d40dac909617af57af67d882879290d49f023c43
Emyli Quadros <babaloogames@gmail.com>
2023-11-20 16:55:11 -0300

    tentando tela cadastro

--- CarolesRestaurant/src/app/app.routes.ts
+++ CarolesRestaurant/src/app/app.routes.ts
@@ -1,5 +1,9 @@
 import { Routes } from '@angular/router';
+import { CadastroComponent } from './cadastro/cadastro.component';
 
 export const routes: Routes = [
-
+  {
+    path: '/cadastro',
+		component: CadastroComponent,
+  }
 ];
