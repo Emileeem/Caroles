c9019fdcee337bcf7dc785c957fcce26ad460803
Emyli Quadros <babaloogames@gmail.com>
2023-11-20 16:40:52 -0300

    iniciando tela de cadastro

---
+++ CarolesRestaurant/src/app/cadastro/cadastro.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-cadastro',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './cadastro.component.html',
+  styleUrl: './cadastro.component.css'
+})
+export class CadastroComponent {
+
+}
