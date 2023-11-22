db3f02d6abdedd2a7b7ab36f9c2cad882a2f2c02
Emyli Quadros <babaloogames@gmail.com>
2023-11-22 15:09:41 -0300

    iniciando a tela inicial do usuário

--- CarolesRestaurant/src/app/app.routes.ts
+++ CarolesRestaurant/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { CadastroComponent } from './cadastro/cadastro.component';
 import { LoginComponent } from './login/login.component';
+import { IndexUserComponent } from './index-user/index-user.component';
 
 export const routes: Routes = [
+  {path: '', component: LoginComponent},
   {path: 'cadastro', component: CadastroComponent},
-  {path: '', component: LoginComponent}
+  {path: 'index', component: IndexUserComponent}
 ];
