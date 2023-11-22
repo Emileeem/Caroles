136ea8eb535a824ee44c3cd57d8213e5d47016f3
Emyli Quadros <babaloogames@gmail.com>
2023-11-22 13:47:29 -0300

    tela de login pronta

--- CarolesRestaurant/src/app/app.routes.ts
+++ CarolesRestaurant/src/app/app.routes.ts
@@ -1,9 +1,8 @@
 import { Routes } from '@angular/router';
 import { CadastroComponent } from './cadastro/cadastro.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
-  {
-    path: '/cadastro',
-		component: CadastroComponent,
-  }
+  {path: 'cadastro', component: CadastroComponent},
+  {path: '', component: LoginComponent}
 ];
