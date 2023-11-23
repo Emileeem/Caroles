8e991951485a2969feec239525cae08d657c0a91
Emyli Quadros <babaloogames@gmail.com>
2023-11-23 15:41:41 -0300

    desistindo desse e recomeçando

--- CarolesRestaurant/src/app/app.routes.ts
+++ CarolesRestaurant/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { CadastroComponent } from './cadastro/cadastro.component';
 import { LoginComponent } from './login/login.component';
 import { IndexUserComponent } from './index-user/index-user.component';
+import { IndexAdminComponent } from './index-admin/index-admin.component';
 
 export const routes: Routes = [
   {path: '', component: LoginComponent},
   {path: 'cadastro', component: CadastroComponent},
-  {path: 'index', component: IndexUserComponent}
+  {path: 'index', component: IndexUserComponent},
+  {path: 'indexAdmin', component: IndexAdminComponent},
 ];
