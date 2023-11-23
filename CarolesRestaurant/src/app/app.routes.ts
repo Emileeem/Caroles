4379ecfaeba76fd2e03d575f86fedd39b4c0331e
Emyli Quadros <babaloogames@gmail.com>
2023-11-23 16:55:35 -0300

    iniciando menu

--- CarolesRestaurant/src/app/app.routes.ts
+++ CarolesRestaurant/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { CadastroComponent } from './cadastro/cadastro.component';
 import { LoginComponent } from './login/login.component';
 import { IndexUserComponent } from './index-user/index-user.component';
 import { IndexAdminComponent } from './index-admin/index-admin.component';
+import { MenuComponent } from './menu/menu.component';
 
 export const routes: Routes = [
   {path: '', component: LoginComponent},
   {path: 'cadastro', component: CadastroComponent},
   {path: 'index', component: IndexUserComponent},
   {path: 'indexAdmin', component: IndexAdminComponent},
+  {path: 'menu', component: MenuComponent},
 ];
