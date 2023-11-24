d2d433d87d9948d4cbcb3908a08d7d035cedf064
LucaFreire <lucasrodifreire@gmail.com>
2023-11-24 15:15:38 -0300

    terminando menu, começando tela de códigos e ingredientes

---
+++ CarolesRestaurant/src/app/ingredients/ingredients.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-ingredients',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './ingredients.component.html',
+  styleUrl: './ingredients.component.css'
+})
+export class IngredientsComponent {
+
+}
