69c9f0d5d4e7107a93b2edca3dfe2af2d307f1d7
LucaFreire <lucasrodifreire@gmail.com>
2023-11-24 16:02:56 -0300

    Criando tela/modal para código de promoção

---
+++ CarolesRestaurant/src/app/code/code.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-code',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './code.component.html',
+  styleUrl: './code.component.css'
+})
+export class CodeComponent {
+
+}
