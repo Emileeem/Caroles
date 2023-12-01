4b006e97e92b7a109b26a1424dbc0f8bb00759ba
Emyli quadros <babaloogames@gmail.com>
2023-12-01 10:24:24 -0300

    iniciando tela de totem

---
+++ CarolesRestaurant/src/app/totem/totem.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-totem',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './totem.component.html',
+  styleUrl: './totem.component.css'
+})
+export class TotemComponent {
+
+}
