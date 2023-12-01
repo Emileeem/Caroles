a6c8be3745cd9adde1bd28a2c20a902c049b373c
Emyli quadros <babaloogames@gmail.com>
2023-12-01 15:56:36 -0300

    iniciando tela de nova promoção

---
+++ CarolesRestaurant/src/app/promocao/promocao.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-promocao',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './promocao.component.html',
+  styleUrl: './promocao.component.css'
+})
+export class PromocaoComponent {
+
+}
