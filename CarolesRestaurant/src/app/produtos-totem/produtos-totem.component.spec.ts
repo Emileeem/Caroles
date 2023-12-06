0218fdd73721453f05447fd62013ed14ad55be00
Emyli quadros <babaloogames@gmail.com>
2023-12-06 13:13:15 -0300

    Iniciando produtos do totem

---
+++ CarolesRestaurant/src/app/produtos-totem/produtos-totem.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ProdutosTotemComponent } from './produtos-totem.component';
+
+describe('ProdutosTotemComponent', () => {
+  let component: ProdutosTotemComponent;
+  let fixture: ComponentFixture<ProdutosTotemComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ProdutosTotemComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(ProdutosTotemComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ CarolesRestaurant/src/app/produtos-totem/produtos-totem.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-produtos-totem',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './produtos-totem.component.html',
+  styleUrl: './produtos-totem.component.css'
+})
+export class ProdutosTotemComponent {
+
+}
