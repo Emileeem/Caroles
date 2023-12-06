7b87286829168609e6da1964d8061c3245e5a24b
Emyli quadros <babaloogames@gmail.com>
2023-12-06 14:36:40 -0300

    Criando tela de carrinho

---
+++ CarolesRestaurant/src/app/carrinho/carrinho.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-carrinho',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './carrinho.component.html',
+  styleUrl: './carrinho.component.css'
+})
+export class CarrinhoComponent {
+
+}
